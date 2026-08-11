ecdf593e06b39bc1f006568b994d58bfc2c82140
liqwave <liqwxve@gmail.com>
2026-08-11 22:56:48 +0300

    fixed taskbar icon not updating in release (AppUserModelID

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 
+#include <shobjidl.h>
+#pragma comment(lib, "shell32.lib")
+
 #include <QApplication>
 #include <QStyleFactory>
 
 int main(int argc, char *argv[]) {
+    SetCurrentProcessExplicitAppUserModelID(L"YourName.QClicker.1");
     QApplication a(argc, argv);
 
     a.setStyle(QStyleFactory::create("Fusion"));
